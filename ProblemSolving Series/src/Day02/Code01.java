63568e266b8fd19537447be353268a743013f78a
Sharwan Jung Kunwar <sharwankunwar07@gmail.com>
2025-12-14 14:50:11 +0545

    added day2

---
+++ ProblemSolving Series/src/Day02/Code01.java
@@ -0,0 +1,20 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class Code01 {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int rev = 0;
+
+        while(num != 0){
+            rev = rev*10+num%10;
+            num/=10;
+        }
+        System.out.println(rev);
+
+    }
+}
