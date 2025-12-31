77f1ef4b08160ea7567118ccb55a15aa840f3afe
Sharwan Jung Kunwar <sharwankunwar07@gmail.com>
2025-12-31 22:19:11 +0545

    updated

--- ProblemSolving Series/src/Day18/Code02.java
+++ ProblemSolving Series/src/Day18/Code02.java
@@ -5,7 +5,11 @@ package Day18;
 public class Code02 {
     static void main(String[] args) {
         int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int sum =0;
 
-        
+        for(int i=0; i<matrix.length; i++){
+            sum+=matrix[i][i]; // same same
+        }
+        System.out.println("Sum : "+sum);
     }
 }
